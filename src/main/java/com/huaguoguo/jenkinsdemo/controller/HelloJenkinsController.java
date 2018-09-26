e38739b9399031276ceb517b7a0921b5ded253ea
liujh <liujianhua@hebao.im>
2018-09-26 11:26:08 +0800

    第一次提交

---
+++ src/main/java/com/huaguoguo/jenkinsdemo/controller/HelloJenkinsController.java
@@ -0,0 +1,14 @@
+package com.huaguoguo.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloJenkinsController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public Object hello(){
+        return "hello jenkins";
+    }
+}
