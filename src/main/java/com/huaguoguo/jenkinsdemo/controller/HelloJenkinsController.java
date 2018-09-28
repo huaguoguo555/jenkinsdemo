40641a20d1671f65365c5f0c268a0cc0c26ef94e
liujh <liujianhua@hebao.im>
2018-09-28 10:25:23 +0800

    加入日志 aaa

--- src/main/java/com/huaguoguo/jenkinsdemo/controller/HelloJenkinsController.java
+++ src/main/java/com/huaguoguo/jenkinsdemo/controller/HelloJenkinsController.java
@@ -1,14 +1,24 @@
 package com.huaguoguo.jenkinsdemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 public class HelloJenkinsController {
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public Object hello(){
-        return "hello jenkins 5:14";
+        String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        logger.info(format);
+        return "hello jenkins aaa";
     }
+
 }
